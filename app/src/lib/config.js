e61351f0e5a19c1944c3654488e6ae3378e0f4af
Adity-aprasad <adityarajprasad882@gmail.com>
2026-05-21 15:21:48 +0530

    fix:added env in frontend and env  package

--- app/src/lib/config.js
+++ app/src/lib/config.js
@@ -1,10 +1,11 @@
 // app/src/lib/config.js
 
+
 export const BACKEND_URL = "http://localhost:4000";  // ← CHANGE THIS
 
-export const SUPABASE_URL = "https://amguwqegeynehatxnfhk.supabase.co";
+export const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
 
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtZ3V3cWVnZXluZWhhdHhuZmhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMDY3MTEsImV4cCI6MjA5NDc4MjcxMX0.BPtnMyORHtYdYyQDxKURP4Tc6UGLaoyDRUmQdAQUUp8";
+export const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const FIXED_LOCATION = {
   lat: 13.0604,
